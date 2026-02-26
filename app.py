2c137451fa39169ea63ebce494b1329efb91b000
Waruenat Rabiabporn <waruenatrabiabporn@gmail.com>
2026-02-26 21:23:34 +0700

    Add initial line chart

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 st.set_page_config(page_title="Sales Dashboard", layout="wide")
 st.title("📊 Sales Dashboard")
 
 df = pd.read_csv("data.csv")
-st.dataframe(df)
+
+fig = px.line(df, x="month", y="sales")
+st.plotly_chart(fig)
