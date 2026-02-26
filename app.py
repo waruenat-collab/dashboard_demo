18afe0f99d7ac1291cfd9f662870754aee17865d
Waruenat Rabiabporn <waruenatrabiabporn@gmail.com>
2026-02-26 21:23:05 +0700

    Load dataset into dashboard

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(page_title="Sales Dashboard", layout="wide")
 st.title("📊 Sales Dashboard")
+
+df = pd.read_csv("data.csv")
+st.dataframe(df)
